e011ab548db7588d9cb6662646d0e6513fc848f8
Avazbek-02 <avazbekmambetov9@gmail.com>
2024-06-04 18:32:03 +0500

    4try

--- api-geteway/migration/000001_create_voting_system.up.sql
+++ api-geteway/migration/000001_create_voting_system.up.sql
@@ -1,7 +1,7 @@
 create table if not exists election(
     id uuid primary key not null,
-    name varchar(40),
-    date TIMESTAMP,
+    name varchar(40) not null,
+    date TIMESTAMP not null,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW()
 );
@@ -48,6 +48,6 @@ create table if not exists public_vote(
 
 create table if not exists votes(
     id uuid primary key not null,
-    candidate_id uuid references candidate(id)
+    candidate_id uuid references candidate(id),
     created_at TIMESTAMP DEFAULT NOW()
 )
--- api-geteway/service/election.go
+++ api-geteway/service/election.go
@@ -1,1 +1,5 @@
 package service
+
+type election struct{
+	
+}
