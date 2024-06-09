93da7d8cba29e8fdd6a4b886e776c19f9dc1d379
Avazbek-02 <avazbekmambetov9@gmail.com>
2024-06-09 12:33:01 +0500

    party and public done

--- api-gateway/api/handler/party.go
+++ api-gateway/api/handler/party.go
@@ -2,43 +2,66 @@ package handler
 
 import (
 	pb "api/genproto"
-	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
-func (h *Handler) CreateParty(ctx *gin.Context)  {
+func (h *Handler) CreateParty(ctx *gin.Context) {
 	party := pb.CreatePartyRequest{}
-	
-	fmt.Println("Worked")
+
 	err := ctx.BindJSON(&party)
-	
-	if err != nil{
-		log.Fatal("Error while create party read json",err)
+	if err != nil {
+		log.Fatal("Error while create party read json", err)
 		return
 	}
-	h.ServiceParty.CreateParty(ctx, &party)
-	ctx.JSON(200,&party)
-}
-
-func (h *Handler) UpdateParty(ctx *gin.Context)  {
-	// id := ctx.Param("id")
-	party := pb.PartyResponse{}
-	err := ctx.BindJSON(&party)
-	if err != nil{
-		log.Fatal("Error while update party and read body",err)
+	partyRes, err := h.ServiceParty.CreateParty(ctx, &party)
+	if err != nil {
+		log.Fatal("Error while create party in handler", err)
 		return
 	}
-	ctx.JSON(200,&party)
+	ctx.JSON(200, partyRes)
 }
 
-func (h *Handler) DeleteParty(ctx *gin.Context)  {
-	// id := ctx.Param("id")
-	ctx.JSON(200,gin.H{"Deleted":"Party is deleted"})
+func (h *Handler) UpdateParty(ctx *gin.Context) {
+	id := ctx.Param("id")
+	party := pb.UpdatePartyRequest{}
+	party.Id = id
+	err := ctx.BindJSON(&party)
+	if err != nil {
+		log.Fatal("Error while update party and read body", err)
+		return
+	}
+	backparty, err := h.ServiceParty.UpdateParty(ctx, &party)
+	if err != nil {
+		log.Fatal("error while update in handler", err)
+		return
+	}
+	ctx.JSON(200, backparty)
 }
 
-func (h *Handler) GetPartyById(ctx *gin.Context)  {
-	// id := ctx.Param("id")
+func (h *Handler) DeleteParty(ctx *gin.Context) {
+	id := ctx.Param("id")
+	party := pb.DeletePartyRequest{
+		Id: id,
+	}
+	_,err := h.ServiceParty.DeleteParty(ctx, &party)
+	if err != nil{
+		log.Fatal("Error while delete in handler",err)
+		return
+	}
+	ctx.JSON(200, gin.H{"Deleted": "Party is deleted"})
+}
 
+func (h *Handler) GetPartyById(ctx *gin.Context) {
+	id := ctx.Param("id")
+	party := pb.GetPartyInfoRequest{
+		Id: id,
+	}
+	getInfo, err := h.ServiceParty.GetPartyInfo(ctx, &party)
+	if err != nil {
+		log.Fatal("error while get info in handler", err)
+		return
+	}
+	ctx.JSON(200, getInfo)
 }
--- api-gateway/api/handler/public.go
+++ api-gateway/api/handler/public.go
@@ -8,24 +8,59 @@ import (
 )
 
 func (h *Handler) GetPublicById(ctx *gin.Context)  {
-	// id := ctx.Param("id")
-
+	id := ctx.Param("id")
+	public := pb.GetPublicInfoRequest{
+		Id: id,
+	}
+	publicRes, err := h.ServicePublic.GetPublicInfo(ctx,&public)
+	if err != nil{
+		log.Fatal("error while get public in handler",err)
+		return
+	}
+	ctx.JSON(200,publicRes)
 }
 
 func (h *Handler) CreatePublic(ctx *gin.Context)  {
-	public := pb.PublicResponse{}
+	public := pb.CreatePublicRequest{}
 	err := ctx.BindJSON(&public)
 	if err != nil{
 		log.Fatal("Error while public create bindjson !",err)
 		return
 	}
-	
+	publicRes,err := h.ServicePublic.CreatePublic(ctx,&public)
+	if err != nil{
+		log.Fatal("error while create public in handler",err)
+		return
+	}
+	ctx.JSON(200,publicRes)
 }
 
 func (h *Handler) DeletePublic(ctx *gin.Context)  {
-	
+	id := ctx.Param("id")
+	public := pb.DeletePublicRequest{}
+	public.Id = id
+
+	_,err := h.ServicePublic.DeletePublic(ctx,&public)
+	if err != nil{
+		log.Fatal("Error while delete public in handler")
+		return
+	}
+	ctx.JSON(200,gin.H{"Deleted":"Successfully deleted"})
 }
 
 func (h *Handler) UpdatePublic(ctx *gin.Context)  {
-	
+	id := ctx.Param("id")
+	public := pb.UpdatePublicRequest{}
+	public.Id = id
+	err := ctx.BindJSON(&public)
+	if err != nil{
+		log.Fatal("error while update public in handler")
+		return
+	}
+	publicRes, err := h.ServicePublic.UpdatePublic(ctx,&public)
+	if err != nil{
+		log.Fatal("Error while update public in handler")
+		return
+	}
+	ctx.JSON(200,publicRes)
 }
